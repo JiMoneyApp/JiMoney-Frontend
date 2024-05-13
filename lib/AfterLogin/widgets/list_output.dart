5deab31217cf62ebcbfe35faaeab1a1e6d2f7081
kenny3715 <91171195+kenny3715@users.noreply.github.com>
2024-05-13 20:48:42 +0800

    still doing with API

--- lib/AfterLogin/widgets/list_output.dart
+++ lib/AfterLogin/widgets/list_output.dart
@@ -1,27 +1,71 @@
+import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:jimoney_frontend/DataBase/costResponse.dart';
 
-class ListOutput extends StatelessWidget {
+class ListOutput extends StatefulWidget {
   const ListOutput({super.key});
 
   @override
+  State<ListOutput> createState() => _ListOutputState();
+}
+
+class _ListOutputState extends State<ListOutput> {
+  CostResponse? costResponse;
+
+  void _fetchApiData() async {
+    var url = 'http://localhost:3000/data';
+    try {
+      var response = await http.get(Uri.parse(url));
+      if (response.statusCode == 200) {
+        print('Server error: ${response.statusCode}');
+        print('Response body: ${response.body}');
+        List<dynamic> jsonResponse = jsonDecode(response.body);
+        List<Data> dataList = jsonResponse.map((data) => Data.fromJson(data)).toList();
+        setState(() {
+          costResponse = CostResponse.fromJson(jsonDecode(response.body));
+          print('Decode Successfully');
+        });
+        
+      } else {
+        print("Failed to load data");
+      }
+    } catch (e) {
+      print("Error: $e");
+    }
+  }
+
+  @override
+ void initState() {
+    // TODO: implement initState
+    super.initState();
+    _fetchApiData();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.maxFinite,
-      decoration: BoxDecoration(
-        shape: BoxShape.rectangle,
-        borderRadius: BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20)),
-        border: Border.all(color: Colors.grey)
-      ),
-      child: ListView(
-        scrollDirection: Axis.vertical,
-        children: <Widget>[
-          Center(
-            child: Text(
-              "11"
-            ),
-          )
-        ],
+    return Scaffold(
+      body: Container(
+        width: double.maxFinite,
+        decoration: BoxDecoration(
+            shape: BoxShape.rectangle,
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20), topRight: Radius.circular(20)),
+            border: Border.all(color: Colors.grey)),
+        child: //costResponse != null
+               ListView.builder(
+                itemCount: costResponse?.data?.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(costResponse?.data?[index].title ?? 'No Title'),
+                    subtitle:
+                        Text(costResponse?.data?[index].createdAt ?? 'No Date'),
+                  );
+                },
+                scrollDirection: Axis.vertical,
+              )
+            //: Center(child: CircularProgressIndicator()),
       ),
     );
   }
